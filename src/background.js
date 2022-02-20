0737dc9e91025616bc66a55ea835b741db38b491
Caiprozect <caiprozect@gmail.com>
2022-02-20 12:31:53 +0900

    change img location

--- src/background.js
+++ src/background.js
@@ -2,4 +2,4 @@ const body = document.querySelector("body");
 
 const randomIdx = Math.floor(Math.random() * 3) + 1;
 
-body.style.backgroundImage = `url("../img/bg${randomIdx}.jpg")`;
+body.style.backgroundImage = `url("img/bg${randomIdx}.jpg")`;
